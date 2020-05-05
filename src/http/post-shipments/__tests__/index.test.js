e9c173289fb2934070c1ae8c3c49cba95b4e630e
Braden Douglass <braden.douglass@gmail.com>
2020-05-05 15:47:29 -0400

    Complete Post Shipment Route.
    Test and finish the post shipment route from a response/persistence
    standpoint. This does not emit an event to be picked up and processed by
    subsequent Lambda invocations.

---
+++ src/http/post-shipments/__tests__/index.test.js
@@ -0,0 +1,43 @@
+const sandbox = require("@architect/sandbox")
+const arc = require('@architect/functions')
+const test = require("tape")
+const tiny = require('tiny-json-http')
+const data = require('./fixtures')
+
+let db // Database Connection
+let end // Server Connection
+
+test('arc.start', async t => {
+  t.plan(1)
+  end = await sandbox.start()
+  db = await arc.tables()
+  t.ok(true, 'Sandbox Started')
+})
+
+test('POST /shipments response', async t => {
+  t.plan(2)
+
+  tiny.post({
+    url: "http://localhost:3333/shipments",
+    data: data
+  }, (err, result) => {
+    if (err) throw err
+    t.ok(result, 'Successful Result Received')
+    t.ok(result.headers['location'], 'Location of Shipment Returned')
+  })
+})
+
+test('POST /shipments data', async t => {
+  t.plan(1)
+
+  result = await db.shipments.get({
+    shipmentId: data.number,
+  })
+  t.ok(result, "Shipment Persisted Successfully")
+})
+
+test('arc.end', async t => {
+  t.plan(1)
+  await end()
+  t.ok(true, 'Sandbox Closed')
+})
