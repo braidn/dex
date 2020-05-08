fdeed1e4db54ed483f3aa1707039fcbce01b2967
Braden Douglass <braden.douglass@gmail.com>
2020-05-08 16:26:17 -0400

    Initial Stub For Warehouse Post.
    Spike out a potential implementation around what a Warehouse post would
    look like. This doesn't really 'massage' the shipment into a body that
    the warehouse could use. Rather it takes the data from the event and
    stuffs it straight down to the warehouse. This also is devoid of unit
    tests for now... ;(

--- src/events/shipment-received/index.js
+++ src/events/shipment-received/index.js
@@ -1,5 +1,42 @@
 // learn more about event functions here: https://arc.codes/primitives/events
 exports.handler = async function subscribe (event) {
-  console.log(JSON.stringify(event, null, 2))
+  const arc = require('@architect/functions')
+  const https = require('https')
+
+  const options = {
+    hostname: 'warehouse1.com',
+    port: 443,
+    path: '/addShipment',
+    method: 'POST',
+    headers: {
+      'AUTHORIZATION': 'BYTE ME',
+      "CONTENT-TYPE": 'application/jsoon'
+    }
+  }
+
+  const shipIt = async ((_req, _context) => {
+    const r = https.request(options, res => {
+      console.log(res)
+      res.on('data', async data => {
+        const db = await arc.tables()
+
+        db.shipments.put({
+          shipmentId: event.shipmentId,
+          type: "Status/Init",
+          createdAt: new Date().toISOString(),
+          ...data
+        })
+      })
+    })
+
+    res.on('error', (e) => {
+      console.log(e)
+    })
+
+    r.write(event.data)
+    r.end()
+  })
+
+  arc.http.async(shipIt)
   return
 }
