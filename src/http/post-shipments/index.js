e63c323e510f9acf4f7b1331d2825b3450823c44
Braden Douglass <braden.douglass@gmail.com>
2020-05-15 15:45:35 -0400

    Actually Publish Event In Shipment Received.
    Publish the event using the arc.events functions.
    This takes a params hash and a callback. From there,
    it sends an event to the function defined in the .arc file.

--- src/http/post-shipments/index.js
+++ src/http/post-shipments/index.js
@@ -1,21 +1,26 @@
 // learn more about HTTP functions here: https://arc.codes/primitives/http
-exports.handler = async function http (req) {
-  let arc = require('@architect/functions')
-  const db = await arc.tables()
-  const body = arc.http.helpers.bodyParser(req)
+exports.handler = async function http(req) {
+  let arc = require("@architect/functions");
+  const db = await arc.tables();
+  const body = arc.http.helpers.bodyParser(req);
+  const event = {
+    name: "shipment-received",
+    payload: { shipmentId: body.number },
+  };
 
   await db.shipments.put({
     shipmentId: body.number,
     type: "Shipment",
     createdAt: new Date().toISOString(),
-    ...body
-  })
+    ...body,
+  });
+  await arc.events.publish(event);
 
   return {
     headers: {
-      'content-type': 'application/json; charset=utf8',
-      'location': `/shipments/${body.number}`
+      "content-type": "application/json; charset=utf8",
+      location: `/shipments/${body.number}`,
     },
     statusCode: 201,
-  }
-}
+  };
+};
