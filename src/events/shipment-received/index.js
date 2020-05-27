ba896d860daed5323e3563553397bd44c4ace170
Braden Douglass <braden.douglass@gmail.com>
2020-05-27 17:37:59 -0400

    Finally Get Tested Queue Function.
    The SNS queue is going to send a list of messages through to each lambda.
    Due to this, the work that's done in these queue lambdas will need to work
    on a list of objects. Even further when using Array functions like .map,
    one needs to wrap the result in a Promise.all to resolve correctly.
    
    JS can sometimes be a conveluted mess, like Array functions and async/await.

--- src/events/shipment-received/index.js
+++ src/events/shipment-received/index.js
@@ -1,42 +1,42 @@
 // learn more about event functions here: https://arc.codes/primitives/events
-exports.handler = async function subscribe (event) {
-  const arc = require('@architect/functions')
-  const https = require('https')
-  try {
-    const shipIt = (async (_req, _context) => {
-      const options = {
-        hostname: 'http://warehouse1.com',
-        port: 443,
-        path: '/addShipment',
-        method: 'POST',
-        headers: {
-          'AUTHORIZATION': 'BYTE ME',
-          "CONTENT-TYPE": 'application/jsoon'
-        }
+exports.handler = async function subscribe(event) {
+  const arc = require("@architect/functions");
+  const got = require('got');
+
+  const events = JSON.stringify(event, null, 2);
+  const { Records } = JSON.parse(events);
+  const db = await arc.tables();
+
+    const shipIt = async (record) => {
+      try {
+        const { body } = await got.post("https://warehouse1.com/addShipment",
+          {
+            json: {
+              shipment: record.shipmentId
+            },
+            responseType: 'json'
+          }
+        )
+
+        await db.shipments.put({
+          shipmentId: record.shipmentId,
+          type: "Status/Init",
+          createdAt: new Date().toISOString(),
+          ...body,
+        });
+      } catch (e) {
+        console.log(e)
       }
-      const r = https.request(options, async res => {
-        res.on('data', async data => {
-          const db = await arc.tables()
+    };
 
-          db.shipments.put({
-            shipmentId: event.data.shipmentId,
-            type: "Status/Init",
-            createdAt: new Date().toISOString(),
-            ...data
-          })
-        })
-        res.on('error', (e) => {
-          console.log(e)
-        })
-      })
-
-      r.write(event.data)
-      r.end()
+    const records = Records.map(async (shipment) => {
+      const {
+        Sns: { Message: data }
+      } = shipment;
+      await shipIt(data);
     })
 
-    arc.http.async(shipIt)
-    return {statusCode: 201}
-  } catch (e) {
-    return {statusCode: 404, error: e}
-  }
-}
+    return await Promise.all(records)
+      .then(_result => ({statusCode: 201}))
+      .catch(e => ({ statusCode: 404, error: e }))
+};
